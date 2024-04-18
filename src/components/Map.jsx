7677181be017cbcdc220b42f6a74496fbd0bca4d
HarperVenom <162710380+HarperVenom@users.noreply.github.com>
2024-04-19 00:38:14 +0300

    smooth trails

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -4,7 +4,7 @@ const Map = ({ universe }) => {
   const elementWidth = 150;
   const elementMarginRight = 100;
   const elementHeight = 50;
-  const elementMarginBot = 40;
+  const elementMarginBot = 50;
   const [branchHeight, setBranchHeight] = useState(0);
 
   const branch = universe.branches[0];
@@ -30,6 +30,9 @@ const Map = ({ universe }) => {
         title.yLevel = getYLevel(title.id);
         return;
       });
+
+      const container = document.querySelector(".trails");
+      container.innerHTML = "";
     });
 
     setTitles(updatedTitles);
@@ -106,19 +109,11 @@ const Map = ({ universe }) => {
 
 export default Map;
 
-const Level = ({ items, elementStyle }) => {
-  return (
-    <div className="level">
-      {items.map((item) => (
-        <Element key={item.id} item={item} style={elementStyle} />
-      ))}
-    </div>
-  );
-};
-
-const Element = ({ item, style }) => {
+const Element = ({ item, style, active }) => {
   const [location, setLocation] = useState(null);
-  const [parentsLocations, setParentsLocations] = useState([]);
+  const [parents, setParents] = useState([]);
+  const [trails, setTrails] = useState([]);
+  // const [active, setActive] = useState(false);
 
   useEffect(() => {
     if (style.branchHeight === 0) return;
@@ -131,32 +126,109 @@ const Element = ({ item, style }) => {
 
     const parentsIds = item.watchAfter;
     if (parentsIds.length === 0) return;
-    const parentsLocations = parentsIds.map((id) => {
+    let parents = [];
+    parentsIds.forEach((id) => {
       const element = document.getElementById(id);
-      // console.log(element);
-      return { left: element.style.left, top: element.style.top };
+      parents.push(element);
     });
-    // console.log(parentsLocations);
-    setParentsLocations(parentsLocations);
+    setParents(parents);
   }, [item, style]);
 
   useEffect(() => {
     const SVG_NS = "http://www.w3.org/2000/svg";
     const container = document.querySelector(".trails");
-    parentsLocations.forEach((parentLocation) => {
-      if (!parentLocation.left || !parentLocation.top) return;
-      // console.log(parentLocation);
-      console.log(parseInt(location.top));
-      const line = document.createElementNS(SVG_NS, "line");
-      line.setAttribute("x1", parseInt(location.left) + 10);
-      line.setAttribute("y1", parseInt(location.top) + style.height / 2);
-      line.setAttribute("x2", parseInt(parentLocation.left) + style.width);
-      line.setAttribute("y2", parseInt(parentLocation.top) + style.height / 2);
-      line.setAttribute("stroke", "white");
+    // container.innerHTML = "";
+    parents
+      .map((parent) => {
+        return { left: parent.style.left, top: parent.style.top };
+      })
+      .forEach((parentLocation) => {
+        if (!parentLocation.left || !parentLocation.top) return;
+        //<path id="lineAC" d="M 100 350 q -50 -100 100 -100"
+        //stroke="blue" fill="none" stroke-width="4"/>
+        const path = document.createElementNS(SVG_NS, "path");
+        const start = {
+          x: parseInt(location.left) + 10,
+          y: parseInt(location.top) + style.height / 2,
+        };
+        const end = {
+          x: parseInt(parentLocation.left) + style.width,
+          y: parseInt(parentLocation.top) + style.height / 2,
+        };
+        const relativeEnd = {
+          x: end.x - start.x,
+          y: end.y - start.y,
+        };
 
-      container.appendChild(line);
+        // const points = [
+        //   { x: start.x, y: start.y },
+        //   { x: start.x - 30, y: start.y },
+        //   { x: start.x - 40, y: start.y - 10 },
+        //   { x: end.x, y: end.y },
+        // ];
+
+        // const pathString = points.reduce((acc, point, index, arr) => {
+        //   if (index === 0) {
+        //     return `M ${point.x} ${point.y}`;
+        //   } else if (index === 1) {
+        //     // First control point
+        //     const cx1 = arr[index - 1].x + (point.x - arr[index - 1].x) / 3;
+        //     const cy1 = arr[index - 1].y + (point.y - arr[index - 1].y) / 3;
+        //     // Second control point
+        //     const cx2 = point.x - (arr[index + 1].x - point.x) / 3;
+        //     const cy2 = point.y - (arr[index + 1].y - point.y) / 3;
+        //     return `${acc} C ${cx1} ${cy1}, ${cx2} ${cy2}, ${point.x} ${point.y}`;
+        //   } else if (index === arr.length - 1) {
+        //     // Last point, no need for control points
+        //     return `${acc} L ${point.x} ${point.y}`;
+        //   } else {
+        //     // Calculate control points based on previous and next points
+        //     const cx1 = arr[index - 1].x + (point.x - arr[index - 1].x) / 3;
+        //     const cy1 = arr[index - 1].y + (point.y - arr[index - 1].y) / 3;
+        //     const cx2 = point.x - (arr[index + 1].x - point.x) / 3;
+        //     const cy2 = point.y - (arr[index + 1].y - point.y) / 3;
+        //     return `${acc} S ${cx1} ${cy1}, ${point.x} ${point.y}`;
+        //   }
+        // }, "");
+
+        // path.setAttribute("d", pathString);
+
+        path.setAttribute(
+          "d",
+          `M ${start.x}
+          ${start.y} c ${-style.marginRight / 2} ${0} ${
+            relativeEnd.x + style.marginRight / 2
+          } ${relativeEnd.y} ${relativeEnd.x} ${relativeEnd.y}`
+        );
+        path.setAttribute("stroke", "rgba(255, 255, 255, 0.2)");
+        path.setAttribute("stroke-width", "3px");
+        path.setAttribute("fill", "none");
+
+        // const line = document.createElementNS(SVG_NS, "line");
+        // line.setAttribute("x1", parseInt(location.left) + 10);
+        // line.setAttribute("y1", parseInt(location.top) + style.height / 2);
+        // line.setAttribute("x2", parseInt(parentLocation.left) + style.width);
+        // line.setAttribute(
+        //   "y2",
+        //   parseInt(parentLocation.top) + style.height / 2
+        // );
+        // line.setAttribute("stroke", "white");
+
+        // setTrails((prev) => [...prev, line]);
+        setTrails((prev) => [...prev, path]);
+
+        container.appendChild(path);
+      });
+  }, [parents]);
+
+  useEffect(() => {
+    // onClick();
+    // console.log("hey " + item.id);
+    trails.forEach((trail) => {
+      if (active) trail.classList.add("active");
+      else trail.classList.remove("active");
     });
-  }, [parentsLocations]);
+  }, [active]);
 
   return (
     <div
@@ -173,7 +245,13 @@ const Element = ({ item, style }) => {
           : null
       }
       onClick={() => {
-        console.log(location.left, location.top);
+        // setActive((prev) => !prev);
+        // console.log(location.left, location.top);
+        // console.log(trails);
+        // console.log(parents);
+        parents.forEach((parent) => {
+          parent.click();
+        });
       }}
     >
       <p className="title">{item.title}</p>
